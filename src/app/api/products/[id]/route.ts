dca44124655f6dbb3d875041b9b96068001aa509
Safa Ghulam Muhammad <161748050+SAFAGHULAM@users.noreply.github.com>
2025-01-03 17:46:44 +0500

    done33

--- src/app/api/products/[id]/route.ts
+++ src/app/api/products/[id]/route.ts
@@ -1,22 +1,10 @@
 import { NextResponse } from 'next/server';
 import { products } from '../../../data/product';
 
-export async function GET(req: Request, context: { params: Record<string, string> }) {
-  const { id } = context.params;
-
-  // Convert ID to a number
-  const productId = parseInt(id, 10);
-  if (isNaN(productId)) {
-    return NextResponse.json({ message: 'Invalid product ID' }, { status: 400 });
-  }
-
-  // Find the product
-  const product = products.find(p => p.id === productId);
-
+export async function GET(req: Request, { params }: { params: { id: string } }) {
+  const product = products.find(p => p.id === parseInt(params.id, 10));
   if (!product) {
     return NextResponse.json({ message: 'Product not found' }, { status: 404 });
   }
-
-  // Return the product
   return NextResponse.json(product);
 }
