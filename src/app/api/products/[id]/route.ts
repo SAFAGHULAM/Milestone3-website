1119fe7b1d92656afcef36663ed925bf1d3fdff9
Safa Ghulam Muhammad <161748050+SAFAGHULAM@users.noreply.github.com>
2025-01-03 20:08:47 +0500

    done234

--- src/app/api/products/[id]/route.ts
+++ src/app/api/products/[id]/route.ts
@@ -1,11 +1,15 @@
 import { NextResponse } from 'next/server';
 import { products } from '../../../data/product';
+import type { RouteHandlerContext } from 'next/server';
 
-export async function GET(req: Request, context: { params: { id: string } }) {
+export async function GET(
+  req: Request, 
+  context: RouteHandlerContext<{ id: string }>
+) {
   const { id } = context.params;
   const product = products.find(p => p.id === parseInt(id, 10));
   if (!product) {
     return NextResponse.json({ message: 'Product not found' }, { status: 404 });
   }
   return NextResponse.json(product);
-}
+}d
